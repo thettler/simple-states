7a147ba574f9fd16ee32f926e38d4db238592434
Tobias Hettler <tobias.hettler@bitbench.dev>
2026-06-04 15:08:13 +0200

    WIP

--- src/State.php
+++ src/State.php
@@ -4,4 +4,40 @@ declare(strict_types=1);
 
 namespace Thettler\SimpleStates;
 
-interface State {}
+use Thettler\SimpleStates\Exceptions\SimpleStateAttributeClassHasNoInvokeMethod;
+use Thettler\SimpleStates\Exceptions\SimpleStateAttributeMissingException;
+use Thettler\SimpleStates\Exceptions\SimpleStateMustBeUsedOnBackedEnumException;
+use Thettler\SimpleStates\Exceptions\SimpleStateNotAllowedTransitionException;
+
+interface State
+{
+    /**
+     * @template T of (\BackedEnum&State)
+     * @param  T  $state
+     * @return T
+     * @throws SimpleStateNotAllowedTransitionException
+     * @throws SimpleStateMustBeUsedOnBackedEnumException
+     * @throws \ReflectionException
+     */
+    public function transitionTo(\BackedEnum&State $state): \BackedEnum&State;
+
+    /**
+     * @throws SimpleStateMustBeUsedOnBackedEnumException
+     * @throws \ReflectionException
+     */
+    public function canTransitionTo(\BackedEnum&State $transitionTo, ...$params): bool;
+
+    /**
+     * @throws SimpleStateMustBeUsedOnBackedEnumException
+     * @throws \ReflectionException
+     */
+    public function getTransitions(...$params): array;
+
+    /**
+     * @throws SimpleStateAttributeMissingException
+     * @throws \ReflectionException
+     * @throws SimpleStateAttributeClassHasNoInvokeMethod
+     * @throws SimpleStateMustBeUsedOnBackedEnumException
+     */
+    public function getStateAttribute(string $attributeName, ...$params): mixed;
+}
