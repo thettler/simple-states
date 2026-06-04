2eee31aae3c881c1ca9f82b2b68ebc1da80cd336
Tobias Hettler <tobias.hettler@bitbench.dev>
2026-06-04 15:28:40 +0200

    Make Attributes Repeatable

--- src/Attributes/StateAttribute.php
+++ src/Attributes/StateAttribute.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Thettler\SimpleStates\Attributes;
 
-#[\Attribute(\Attribute::TARGET_CLASS_CONSTANT)]
+#[\Attribute(\Attribute::TARGET_CLASS_CONSTANT | \Attribute::IS_REPEATABLE)]
 class StateAttribute
 {
     public function __construct(
